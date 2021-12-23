40e9165ba207edcaca60fb6703fb62eb6a5b4a48
Rakibul Islam <rakibulislam.ruian@gmail.com>
2021-12-23 15:17:57 +0600

    call the weather api to get the current api

--- app/src/main/java/bd/ac/ru/cse3162/MainActivity.kt
+++ app/src/main/java/bd/ac/ru/cse3162/MainActivity.kt
@@ -12,8 +12,16 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.gms.location.*
 import java.util.*
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var fusedlocation: FusedLocationProviderClient
@@ -39,6 +47,12 @@ class MainActivity : AppCompatActivity() {
                         var long = location.longitude.toString()
 
                         if (lat != null && long != null) {
+                            // Getting JSON data from api
+                            weather_btn.setOnClickListener {
+                                getCurrentJsonData(lat, long)
+                            }
+
+                            // Update info to the UI
                             var (cityName, country) = getAddress(lat.toDouble(), long.toDouble())
                             city.text = cityName + ", "+ country
                             latitude.text = lat
@@ -112,4 +126,45 @@ class MainActivity : AppCompatActivity() {
 
         return Address(adress.get(0).locality, adress.get(0).countryName)
     }
+
+    private fun getCurrentJsonData(lat: String, long: String) {
+        val API_KEY = "331ce978da5d446d953f327f3f948f5f"
+        val queue = Volley.newRequestQueue(this)
+
+        val url =
+            "https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${long}&key=${API_KEY}&include=minutely"
+
+        try {
+            val jsonRequest = JsonObjectRequest(
+                Request.Method.GET, url, null,
+                Response.Listener { response ->
+                    setValues(response)
+                },
+                Response.ErrorListener {
+                    Toast.makeText(
+                        this,
+                        "Please turn on internet connection",
+                        Toast.LENGTH_LONG
+                    ).show()
+                })
+
+
+            queue.add(jsonRequest)
+        } catch (e: Exception) {
+            Toast.makeText(
+                this,
+                "ERROR" + e.message,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun setValues(response: JSONObject) {
+        var tempCel = response.getJSONArray("data").getJSONObject(0).getInt("temp")
+        temp_c.text =  "${tempCel} °C"
+        tempF.text =  "${tempCel * 1.8 + 32} °F"
+        var hdt = response.getJSONArray("data").getJSONObject(0).getString("rh")
+        humidity.text =  hdt + "%"
+    }
+
 }
